465b02f34c39577cd301d11df22f313f925228e2
tlsappointements-oss <tls.appointements@gmail.com>
2026-01-21 13:19:41 +0100

    k

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-function filterCars(){
- let q=document.getElementById('search').value.toLowerCase();
- document.querySelectorAll('.card').forEach(c=>{
-  c.style.display=c.dataset.name.includes(q)?'block':'none';
- });
-}
