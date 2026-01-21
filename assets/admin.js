9e368f44c7d985a07ff89276cf9251a8f61a8253
tlsappointements-oss <tls.appointements@gmail.com>
2026-01-21 11:06:09 +0100

    Create admin.js

---
+++ assets/admin.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnp4fC2_cEw04ydtWOwYgVzRUsqScufFs",
+  authDomain: "cars-website-558c0.firebaseapp.com",
+  projectId: "cars-website-558c0"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+/* 🔐 PROTECT PAGE */
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
+
+/* LOGOUT */
+document.getElementById("logout").onclick = () => {
+  signOut(auth).then(() => location.href = "login.html");
+};
